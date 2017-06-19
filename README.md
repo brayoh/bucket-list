eae6082dfb07723e03b9ee67629c4a10b5da7952
brayoh <bkuriakb@gmail.com>
2017-06-19 11:25:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# andela-bootcamp
+Pre Bootcamp Technical Assesment Test
+
---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+(function() {
+    var header = document.getElementsByTagName("header")[0];
+    var height = window.innerHeight;
+    header.style.height = height + 'px';
+}());
