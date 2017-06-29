da2e487efa0b7268f4e460a5ffa314b18ceb90e0
brayoh <bkuriakb@gmail.com>
2017-06-29 11:24:09 +0300

    Add logger to app, change modal label, add default case for bucketlist modal edit mode

--- app.py
+++ app.py
@@ -1,4 +1,29 @@
+import logging.config
 from app import app
+
+
+LOG_LEVEL = logging.INFO
+
+logging.config.dictConfig(dict(
+    version=1,
+    disable_existing_loggers=False,
+    formatters={
+        'f': {
+            'format': '%(asctime)s - %(process)d - %(name)s:%(lineno)d - %(levelname)-8s - %(message)s'}},
+    handlers={
+        'h': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'f',
+            'level': LOG_LEVEL,
+        }
+    },
+    root={
+        'handlers': ['h'],
+        'level': LOG_LEVEL,
+    },
+))
+
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     app.run()
